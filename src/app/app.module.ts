8c1a1949ac2eaef763c0e37f3d200c432066fffd
CaoXuanQuy1985 <cun.kin1985.working@gmail.com>
2020-07-08 07:36:15 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageSliceComponent } from './image-slice/image-slice.component';
+import { ImgSliderComponent } from './img-slider/img-slider.component';
+import { ImageSlideComponent } from './img-slider/image-slide/image-slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSliceComponent,
+    ImgSliderComponent,
+    ImageSlideComponent
   ],
   imports: [
     BrowserModule
